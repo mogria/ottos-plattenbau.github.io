ea5f2ffda41c1094f57851cd44a94c6bb60c379a
Mogria <m0gr14@gmail.com>
2022-01-22 10:26:28 +0100

    database: add more tables for projects and roles

--- database.sql
+++ database.sql
@@ -11,23 +11,44 @@ CREATE TABLE IF NOT EXISTS members (
   fullname     varchar(255) NOT NULL,
   address      varchar(255) NOT NULL,
   email        varchar(255) NOT NULL,
+  scm_username varchar(255) NOT NULL,
+  homepage     text,
+  public_key   text,
   created_at   timestamp DEFAULT NOW(),
   updated_at   timestamp DEFAULT current_timestamp,
   deleted_at   timestamp DEFAULT NULL
   -- picture
 );
 
-CREATE TABLE IF NOT EXISTS projects (
+CREATE TABLE IF NOT EXISTS roles (
   id           SERIAL NOT NULL PRIMARY KEY,
-  projectname  varchar(255) NOT NULL,
+  rolename     varchar(255) NOT NULL,
   created_at   timestamp DEFAULT NOW(),
   updated_at   timestamp DEFAULT current_timestamp,
   deleted_at   timestamp DEFAULT NULL
 );
 
-CREATE TABLE IF NOT EXISTS project_team (
-  member_id    SERIAL NOT NULL,
+
+CREATE TABLE IF NOT EXISTS member_roles (
+  member_id    SERIAL NOT NULL PRIMARY KEY,
+  role_id      SERIAL NOT NULL PRIMARY KEY,
+  -- TODO: UNIQUE-KEY, cascading...?
+);
+
+CREATE TABLE IF NOT EXISTS projects (
+  id           SERIAL NOT NULL PRIMARY KEY,
+  projectname  varchar(255) NOT NULL,
+  homepage     text, -- URL to page explaining project
+  scm          text, -- URL To source code management page
+  created_at   timestamp DEFAULT NOW(),
+  updated_at   timestamp DEFAULT current_timestamp,
+  deleted_at   timestamp DEFAULT NULL
+);
+
+CREATE TABLE IF NOT EXISTS project_members (
   project_id   SERIAL NOT NULL,
+  member_id    SERIAL NOT NULL,
+  role_id      SERIAL DEFAULT NULL, -- role in project
   created_at   timestamp DEFAULT NOW(),
   updated_at   timestamp DEFAULT current_timestamp,
   deleted_at   timestamp DEFAULT NULL
